22243c0153c303e80f3f54e2ff2dcf289cdd76e3
shekinah <nyamburamary1212@gmail.com>
2023-09-15 16:24:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    let button = document.getElementById('button');
+  
+    button.addEventListener('click', function() {
+      // Your event handler logic here
+    });
+  }
+  
